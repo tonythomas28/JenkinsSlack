adbcf4e9b3780cfaa0b76d9c442199630b31e65d
Tony Thomas <tony.thomas@turtlemint.com>
2019-04-22 17:58:46 +0530

    Env Var

--- web.rb
+++ web.rb
@@ -13,6 +13,7 @@ post '/' do
   return [403, "No slack token setup"] unless slack_token = ENV['SLACK_TOKEN']
   return [403, "No jenkins url setup"] unless jenkins_url= ENV['JENKINS_URL']
   return [403, "No jenkins token setup"] unless jenkins_token= ENV['JENKINS_TOKEN']
+  return [403, "No jenkins token setup"] unless jenkins_short_url= ENV['JENKINS_SHORT_URL']
 
   # Verify slack token matches environment variable
   return [401, "No authorized for this command"] unless slack_token == params['token']
